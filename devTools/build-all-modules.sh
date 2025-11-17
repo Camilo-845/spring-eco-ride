e6d2292ba1a79511e26ea4c04f09d6f919e089bf
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-11-16 23:08:16 -0500

    feat: dev-dedicated docker compose
    - Crea script para hacer build de todos los servicios
    - Crea un Dockerfile.dev para cada servicio con tal de omitir la etapa
    de build
    - docker-compsoe.dev.yml para usar Dockerfile.dev de cada servicio

---
+++ devTools/build-all-modules.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Building all modules..."
+
+echo "Building module: config-server (in background)"
+(cd config-service && ./gradlew bootJar -x test) &
+
+echo "Building module: api-gateway (in background)"
+(cd api-gateway && ./gradlew bootJar -x test) &
+
+wait
+
+echo "Build completed"

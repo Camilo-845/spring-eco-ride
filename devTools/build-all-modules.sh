b2b8693e656023c3348519c76d0073ba7f7ba5da
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-11-20 18:07:08 -0500

    feat: payment-service inital configuration

--- devTools/build-all-modules.sh
+++ devTools/build-all-modules.sh
@@ -8,6 +8,9 @@ echo "Building module: config-server (in background)"
 echo "Building module: api-gateway (in background)"
 (cd api-gateway && ./gradlew bootJar -x test) &
 
+echo "Building module: payment-service (in background)"
+(cd payment-service && ./gradlew bootJar -x test) &
+
 wait
 
 echo "Build completed"

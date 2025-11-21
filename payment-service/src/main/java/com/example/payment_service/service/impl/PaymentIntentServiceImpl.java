dede3bd142322fff9e84bf4b8ce465d027c11ec2
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-11-20 21:14:32 -0500

    feat: createPaymentIntent endpoint

--- payment-service/src/main/java/com/example/payment_service/service/impl/PaymentIntentServiceImpl.java
+++ payment-service/src/main/java/com/example/payment_service/service/impl/PaymentIntentServiceImpl.java
@@ -1,17 +1,37 @@
 package com.example.payment_service.service.impl;
 
+import java.time.Instant;
+
+import org.springframework.stereotype.Service;
+
 import com.example.payment_service.dto.request.CreatePaymentIntent;
 import com.example.payment_service.dto.response.PaymentIntentResponse;
+import com.example.payment_service.mapper.PaymentIntentMapper;
+import com.example.payment_service.model.PaymentIntent;
+import com.example.payment_service.repository.PaymentIntentRepository;
 import com.example.payment_service.service.PaymentIntentService;
 
 import reactor.core.publisher.Mono;
 
+@Service
 public class PaymentIntentServiceImpl implements PaymentIntentService {
 
+  private final PaymentIntentRepository paymentIntentRepository;
+  private final PaymentIntentMapper paymentIntentMapper;
+
+  public PaymentIntentServiceImpl(PaymentIntentMapper paymentIntentMapper,
+      PaymentIntentRepository paymentIntentRepository) {
+    this.paymentIntentMapper = paymentIntentMapper;
+    this.paymentIntentRepository = paymentIntentRepository;
+  }
+
   @Override
-  public Mono<PaymentIntentResponse> create(CreatePaymentIntent paymentIntent) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'create'");
+  public Mono<PaymentIntentResponse> create(CreatePaymentIntent paymentIntentRequest) {
+    PaymentIntent paymentIntent = paymentIntentMapper.toEntity(paymentIntentRequest);
+    paymentIntent.setCreatedAt(Instant.now());
+    paymentIntent.setUpdatedAt(Instant.now());
+
+    return paymentIntentRepository.save(paymentIntent).map(paymentIntentMapper::toDto);
   }
 
   @Override

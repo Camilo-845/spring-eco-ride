aa2db2b561c95699700e910d2f9a141a800a05c0
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-11-22 13:43:14 -0500

    feat: payment-service charge&refund create endpoints

--- payment-service/src/main/java/com/example/payment_service/controller/PaymentIntentController.java
+++ payment-service/src/main/java/com/example/payment_service/controller/PaymentIntentController.java
@@ -1,15 +1,20 @@
 package com.example.payment_service.controller;
 
-import jakarta.validation.Valid;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.payment_service.service.PaymentIntentService;
+import com.example.payment_service.dto.request.ChargeRequest;
 import com.example.payment_service.dto.request.PaymentIntentRequest;
+import com.example.payment_service.dto.request.RefundRequest;
+import com.example.payment_service.dto.response.ChargeResponse;
 import com.example.payment_service.dto.response.PaymentIntentResponse;
+import com.example.payment_service.dto.response.RefundResponse;
+import com.example.payment_service.service.ChargeService;
+import com.example.payment_service.service.PaymentIntentService;
+import com.example.payment_service.service.RefundService;
 
 import jakarta.validation.Valid;
 import reactor.core.publisher.Mono;
@@ -19,14 +24,31 @@ import reactor.core.publisher.Mono;
 public class PaymentIntentController {
 
   private final PaymentIntentService paymentIntentService;
+  private final ChargeService chargeService;
+  private final RefundService refundServie;
 
-  public PaymentIntentController(PaymentIntentService paymentIntentService) {
+  public PaymentIntentController(PaymentIntentService paymentIntentService, ChargeService chargeService,
+      RefundService refundServie) {
     this.paymentIntentService = paymentIntentService;
+    this.chargeService = chargeService;
+    this.refundServie = refundServie;
   }
 
   @PostMapping("/intents")
   public Mono<PaymentIntentResponse> createPaymentIntent(@Valid @RequestBody PaymentIntentRequest request) {
     return paymentIntentService.create(request);
+  }
+
+  @PostMapping("/capture/{intentId}")
+  public Mono<ChargeResponse> createCharge(@Valid @RequestBody ChargeRequest request, @PathVariable String intentId) {
+    Mono<ChargeResponse> charge = chargeService.create(intentId, request);
+    // Lanzar evento PaymentAuthorized se se confirmo el pago corretamente
+    return charge;
+  }
+
+  @PostMapping("/refund/{chargeId}")
+  public Mono<RefundResponse> createRefund(@Valid @RequestBody RefundRequest request, @PathVariable String chargeId) {
+    return refundServie.create(chargeId, request);
   }
 
 }

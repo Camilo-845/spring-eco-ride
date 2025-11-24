f0dd6c7cd11f796b4ab59f5a13e6ccc3714bbcb3
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-11-24 01:03:47 -0500

    feat: trip-service trip, location mappers

--- trip-service/src/main/java/com/example/trip_service/mapper/TripMapper.java
+++ trip-service/src/main/java/com/example/trip_service/mapper/TripMapper.java
@@ -1,6 +1,7 @@
 package com.example.trip_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.example.trip_service.controller.dto.request.TripRequest;
 import com.example.trip_service.controller.dto.response.TripResponse;
@@ -10,5 +11,10 @@ import com.example.trip_service.model.Trip;
 public interface TripMapper {
   TripResponse toDto(Trip trip);
 
+  @Mapping(target = "createdAt", ignore = true)
+  @Mapping(target = "updatedAt", ignore = true)
+  @Mapping(target = "driverId", ignore = true)
+  @Mapping(target = "seatsAvailable", ignore = true)
+  @Mapping(target = "id", ignore = true)
   Trip toEntity(TripRequest tripRequest);
 }

f9ff8eb22090025a1ade73aff0616add1674ac18
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-11-23 23:48:16 -0500

    feat: trip-service reservation Service Not null  constrains and logic chages

--- trip-service/src/main/java/com/example/trip_service/service/ReservationService.java
+++ trip-service/src/main/java/com/example/trip_service/service/ReservationService.java
@@ -1,13 +1,12 @@
 package com.example.trip_service.service;
 
-import com.example.trip_service.controller.dto.request.ReservationRequest;
 import com.example.trip_service.controller.dto.response.ReservationResponse;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ReservationService {
-  Mono<ReservationResponse> create(String tripId, ReservationRequest request);
+  Mono<ReservationResponse> create(String tripId, String subId);
 
   Mono<ReservationResponse> getById(String id);
 
--- trip-service/src/main/java/com/example/trip_service/service/impl/ReservationServiceImpl.java
+++ trip-service/src/main/java/com/example/trip_service/service/impl/ReservationServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
-import com.example.trip_service.controller.dto.request.ReservationRequest;
 import com.example.trip_service.controller.dto.response.ReservationResponse;
 import com.example.trip_service.controller.dto.response.TripResponse;
 import com.example.trip_service.exceptions.ResourceNotFoundException;
@@ -15,6 +14,7 @@ import com.example.trip_service.repository.ReservationRepository;
 import com.example.trip_service.service.ReservationService;
 import com.example.trip_service.service.TripService;
 
+import jakarta.validation.constraints.NotNull;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -33,27 +33,29 @@ public class ReservationServiceImpl implements ReservationService {
   }
 
   @Override
-  public Mono<ReservationResponse> create(String tripId, ReservationRequest request) {
-    // Identificator temporal mientras se obtiene por el jwt
-    String subId = "indentificador_temporal";
-
+  public Mono<ReservationResponse> create(@NotNull String tripId, @NotNull String subId) {
     // Buscar Existencia e identificador en servicio Passager
+    // Mono<PassagerResponse> passager = passagerClient.findBySubId(subId);
     Mono<TripResponse> passager = tripService.findById(tripId);
 
     Mono<TripResponse> trip = tripService.findById(tripId);
 
     return Mono.zip(trip, passager)
         .flatMap(result -> {
-          Reservation reservation = reservationMapper.toEntity(request);
-          reservation.setCreatedAt(Instant.now());
-          reservation.setUpdatedAt(Instant.now());
-          reservation.setTripId(UUID.fromString(result.getT1().id()));
+          Reservation reservation = Reservation.builder()
+              .tripId(UUID.fromString(result.getT1().id()))
+              // .passagerId(result.getT2().id());
+              .passagerId(UUID.fromString(result.getT2().id()))
+              .createdAt(Instant.now())
+              .createdAt(Instant.now())
+              .build();
+
           return reservationRepository.save(reservation);
         }).map(reservationMapper::toDto);
   }
 
   @Override
-  public Mono<ReservationResponse> getById(String id) {
+  public Mono<ReservationResponse> getById(@NotNull String id) {
     UUID reservationUuid = UUID.fromString(id);
     return reservationRepository.findById(reservationUuid)
         .switchIfEmpty(Mono.error(new ResourceNotFoundException("reservation", "id", id)))
@@ -61,7 +63,7 @@ public class ReservationServiceImpl implements ReservationService {
   }
 
   @Override
-  public Flux<ReservationResponse> getByTrip(String tripId) {
+  public Flux<ReservationResponse> getByTrip(@NotNull String tripId) {
     UUID tripUuid = UUID.fromString(tripId);
     return tripService.findById(tripId)
         .flatMapMany(result -> {

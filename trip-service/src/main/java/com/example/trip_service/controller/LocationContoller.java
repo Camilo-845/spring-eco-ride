ad22916956abe4b04b892104ff9dc44fe122dedb
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-11-24 01:06:06 -0500

    feat: trip-service create location controller endpoints

---
+++ trip-service/src/main/java/com/example/trip_service/controller/LocationContoller.java
@@ -0,0 +1,35 @@
+package com.example.trip_service.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.trip_service.controller.dto.request.LocationRequest;
+import com.example.trip_service.controller.dto.response.LocationResponse;
+import com.example.trip_service.service.LocationService;
+
+import jakarta.validation.Valid;
+import reactor.core.publisher.Mono;
+
+@RestController("/locations")
+public class LocationContoller {
+
+  private final LocationService locationService;
+
+  public LocationContoller(LocationService locationService) {
+    this.locationService = locationService;
+  }
+
+  @PostMapping("/")
+  public Mono<LocationResponse> createLocation(@Valid @RequestBody LocationRequest request) {
+    return locationService.create(request);
+  }
+
+  @PutMapping("/{id}")
+  public Mono<LocationResponse> updateLocation(@PathVariable Long id, @Valid @RequestBody LocationRequest request) {
+    return locationService.update(id, request);
+  }
+
+}
--- trip-service/src/main/java/com/example/trip_service/service/impl/LocationServiceImpl.java
+++ trip-service/src/main/java/com/example/trip_service/service/impl/LocationServiceImpl.java
@@ -1,13 +1,18 @@
 package com.example.trip_service.service.impl;
 
+import java.time.Instant;
+
 import org.springframework.stereotype.Service;
 
+import com.example.trip_service.controller.dto.request.LocationRequest;
 import com.example.trip_service.controller.dto.response.LocationResponse;
 import com.example.trip_service.exceptions.ResourceNotFoundException;
 import com.example.trip_service.mapper.LocationMapper;
+import com.example.trip_service.model.Location;
 import com.example.trip_service.repository.LocationRepository;
 import com.example.trip_service.service.LocationService;
 
+import jakarta.validation.constraints.NotNull;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -32,6 +37,24 @@ public class LocationServiceImpl implements LocationService {
   @Override
   public Flux<LocationResponse> getAll() {
     return locationRepository.findAll().map(locationMapper::toDto);
+  }
+
+  @Override
+  public Mono<LocationResponse> create(LocationRequest request) {
+    Location location = locationMapper.toEntity(request);
+    location.setCreatedAt(Instant.now());
+    location.setUpdatedAt(Instant.now());
+
+    return locationRepository.save(location).map(locationMapper::toDto);
+  }
+
+  @Override
+  public Mono<LocationResponse> update(@NotNull Long id, LocationRequest request) {
+    return getById(id)
+        .flatMap(result -> {
+          Location location = locationMapper.toEntity(request);
+          return locationRepository.save(location).map(locationMapper::toDto);
+        });
   }
 
 }

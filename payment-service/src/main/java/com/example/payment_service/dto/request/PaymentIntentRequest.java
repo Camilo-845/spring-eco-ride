028169ef718fbe10af279e9bfbc61bd0149f02a8
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-11-25 23:21:59 -0500

    builder on dto PaymentIntentRequest

--- payment-service/src/main/java/com/example/payment_service/dto/request/PaymentIntentRequest.java
+++ payment-service/src/main/java/com/example/payment_service/dto/request/PaymentIntentRequest.java
@@ -1,7 +1,9 @@
 package com.example.payment_service.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
+@Builder
 public record PaymentIntentRequest(
     @NotBlank(message = "reservationId is required") String reservationId,
     @NotBlank(message = "amount is required") Double amount,

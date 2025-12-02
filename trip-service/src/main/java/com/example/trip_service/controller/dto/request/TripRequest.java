0364f6c3c9af10f8394e4da7627bb17dfcf0189e
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-12-01 21:27:41 -0500

    refactor: remove unused imports

--- trip-service/src/main/java/com/example/trip_service/controller/dto/request/TripRequest.java
+++ trip-service/src/main/java/com/example/trip_service/controller/dto/request/TripRequest.java
@@ -2,7 +2,6 @@ package com.example.trip_service.controller.dto.request;
 
 import java.time.Instant;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record TripRequest(@NotNull(message = "origin id required") Long origin,

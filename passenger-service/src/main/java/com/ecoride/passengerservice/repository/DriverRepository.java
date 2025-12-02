ac2d780ec20f916850f2e2bc8578b002f5be1016
CamiloSar <83511143+Camilo-845@users.noreply.github.com>
2025-12-02 11:11:31 -0500

    feat/getByPassengerIdEndpoint (#33)
    * feat: ApiExceiption exceiption Handler
    
    * feat: driver repository method findBByPassengerId
    
    * feat: Driver Service getByPasengerId method implementation
    
    * feat: driver controller getByPassengerId endpoint

--- passenger-service/src/main/java/com/ecoride/passengerservice/repository/DriverRepository.java
+++ passenger-service/src/main/java/com/ecoride/passengerservice/repository/DriverRepository.java
@@ -1,9 +1,15 @@
 package com.ecoride.passengerservice.repository;
 
 import com.ecoride.passengerservice.model.Driver;
+
+import reactor.core.publisher.Mono;
+
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import java.util.UUID;
 
 public interface DriverRepository extends ReactiveCrudRepository<Driver, UUID> {
+  @Query("SELECT * FROM drivers WHERE passenger_id = :passengerId")
+  public Mono<Driver> findByPassengerId(UUID passengerId);
 }

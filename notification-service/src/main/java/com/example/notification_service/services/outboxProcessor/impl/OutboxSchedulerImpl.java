9934088864a380cbded4ff0da79fd24a503c73db
CamiloSar <camilo.sarmiento.amado@gmail.com>
2026-01-01 22:18:02 -0500

    feat: outbox scheduler class methods and implementation

---
+++ notification-service/src/main/java/com/example/notification_service/services/outboxProcessor/impl/OutboxSchedulerImpl.java
@@ -0,0 +1,31 @@
+package com.example.notification_service.services.outboxProcessor.impl;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.example.notification_service.services.outboxProcessor.OutboxProcessor;
+import com.example.notification_service.services.outboxProcessor.OutboxScheduler;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class OutboxSchedulerImpl implements OutboxScheduler {
+  private final OutboxProcessor outboxProcessor;
+
+  public OutboxSchedulerImpl(OutboxProcessor outboxProcessor) {
+    this.outboxProcessor = outboxProcessor;
+  }
+
+  @Override
+  @Scheduled(fixedDelay = 10000)
+  public void runOutboxJob() {
+    outboxProcessor.getBatchToProcess()
+        .flatMap(outbox -> {
+          return outboxProcessor.processOutbox(outbox)
+              .onErrorResume(e -> {
+                // Log de error en proceso
+                return Mono.empty();
+              });
+        }).subscribe();
+  }
+}

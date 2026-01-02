1e5b4a36c9eb48a09a33df06e632aab62eb235f5
CamiloSar <camilo.sarmiento.amado@gmail.com>
2026-01-01 22:08:49 -0500

    feat: outbox reporsitory method fin all pending outbox

--- notification-service/src/main/java/com/example/notification_service/repositories/OutboxRepository.java
+++ notification-service/src/main/java/com/example/notification_service/repositories/OutboxRepository.java
@@ -1,9 +1,22 @@
 package com.example.notification_service.repositories;
 
+import java.time.Instant;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import com.example.notification_service.models.Outbox;
 
-public interface OutboxRepository extends ReactiveCrudRepository<Outbox, Long> {
+import reactor.core.publisher.Flux;
 
+public interface OutboxRepository extends ReactiveCrudRepository<Outbox, Long> {
+  @Query("""
+          SELECT * FROM outboxes
+          WHERE status = :statusId
+          AND (scheduled_at IS NULL OR scheduled_at <= :now)
+          ORDER BY created_at ASC
+          LIMIT :#{#pageable.pageSize} OFFSET :#{#pageable.offset}
+      """)
+  Flux<Outbox> findAllPending(Integer statusId, Instant now, Pageable pageable);
 }

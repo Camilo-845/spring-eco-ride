a92daca12f4c89ab5661fed3ffc6442f8f83b782
CamiloSar <camilo.sarmiento.amado@gmail.com>
2026-01-09 18:25:39 -0500

    feat: api-gateway notification-service routing

--- notification-service/src/main/java/com/example/notification_service/controllers/OutboxController.java
+++ notification-service/src/main/java/com/example/notification_service/controllers/OutboxController.java
@@ -2,6 +2,8 @@ package com.example.notification_service.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.notification_service.dtos.response.OutboxResponse;
 import com.example.notification_service.services.OutboxService;
@@ -9,6 +11,8 @@ import com.example.notification_service.services.OutboxService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@RestController
+@RequestMapping("/outboxes")
 public class OutboxController {
   private final OutboxService outboxService;
 
